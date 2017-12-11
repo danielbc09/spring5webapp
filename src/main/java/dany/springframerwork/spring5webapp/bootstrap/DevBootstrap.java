0d1122b6a661420d3aa672631019c16e0791d14f
Your Name <you@example.com>
2017-12-10 23:25:29 -0500

    Assig add publisher Entity

--- src/main/java/dany/springframerwork/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/dany/springframerwork/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package dany.springframerwork.spring5webapp.bootstrap;
 
 import dany.springframerwork.spring5webapp.model.Author;
 import dany.springframerwork.spring5webapp.model.Book;
+import dany.springframerwork.spring5webapp.model.Publisher;
 import dany.springframerwork.spring5webapp.repositories.AuthorRepository;
 import dany.springframerwork.spring5webapp.repositories.BookRepository;
+import dany.springframerwork.spring5webapp.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,28 +15,38 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent>{
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     private void initData(){
 
         //Eric
         Author eric =  new Author("Eric", "Evans");
-        Book ddd = new Book("Domain Drivern Design", "1234", "Harper Collins");
+        Publisher harperCollins = new Publisher("Harper_Collins", "1234567");
+        Book ddd = new Book("Domain Drivern Design", "1234", harperCollins);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
+
         authorRepository.save(eric);
+        publisherRepository.save(harperCollins);
         bookRepository.save(ddd);
-        
+
+
         //Rod
         Author rod =  new Author("Rod", "Jhonson");
-        Book noEJB = new Book("J2EE Development without EJB", "23444", "Worx");
+        Publisher worx = new Publisher("worx", "1224 Philli Lane");
+        Book noEJB = new Book("J2EE Development without EJB", "23444", worx);
         rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
         authorRepository.save(rod);
+        publisherRepository.save(worx);
         bookRepository.save(noEJB);
+
     }
 
     @Override
---
+++ src/main/java/dany/springframerwork/spring5webapp/repositories/PublisherRepository.java
@@ -0,0 +1,7 @@
+package dany.springframerwork.spring5webapp.repositories;
+
+import dany.springframerwork.spring5webapp.model.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long>{
+}
